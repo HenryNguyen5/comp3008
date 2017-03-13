0132b53bda9d13c19af142b95bd9f6232d7083e2
henrynguyen5 <ngu.henry@hotmail.com>
2017-03-12 20:01:27 -0400

    database implementation

--- lib/password.ts
+++ lib/password.ts
@@ -1,24 +1,21 @@
 //Interface stores the 3 passwords for the user, and what services they're for
+import * as passGens from './generator';
 const services = ['Email', 'Facebook', 'Bank', 'Amazon', 'eBay'];
+const shapes = Object.keys(passGens);
+
+interface Shapes {
+    shape: string;
+}
 
 interface PwdScheme {
     service: string;
-    password: string;
     attempts: number;
+    shapes: Array<Shapes>;
 }
 interface PasswordObj {
     pw1: PwdScheme;
     pw2: PwdScheme;
     pw3: PwdScheme;
-}
-
-function createPwdScheme(): PwdScheme {
-    const randomService = randomArrayElement(services);
-    return {
-        service: randomService,
-        password: randomPass(),
-        attempts: 3
-    };
 }
 
 function createPasswordObj(): PasswordObj {
@@ -29,15 +26,33 @@ function createPasswordObj(): PasswordObj {
     };
 }
 
+function createPwdScheme(): PwdScheme {
+    const randomService = randomArrayElement(services);
+    return {
+        service: randomService(),
+        shapes: createRandomShapes(),
+        attempts: 3
+    };
+}
+
+const createRandomShapes = function createRandomShapes():Array<Shapes> {
+    const numOfShapes = randomInteger(2,4);
+    const shapeArr: Array<Shapes> = [];
+    for (let i = 0; i < numOfShapes; i++) {
+        const randomShape = passGens[randomArrayElement(shapes)];
+        console.log(`Shape selected: ${randomShape}`);
+        shapeArr.push(randomShape);
+    }
+    return shapeArr;
+};
+
 const randomArrayElement = function(arr:Array<any>):any {
     const randomIdx = randomInteger(0, arr.length -1);
     return arr[randomIdx];
-}
+};
 
 const randomInteger = function (min:number, max:number):number{
     return Math.floor(Math.random() * (min - max) + min);
-}
+};
 
-const randomPass = function() {
-    return 'a';
-}
+export { createPasswordObj, PasswordObj, PwdScheme }
